e0d413a6b99d0980c51d5dba99f450f3f54acf99
Ignadimas <ignadimas@gmail.com>
2020-12-02 13:06:14 +0700

    revisi

--- app/src/main/java/com/tubesb/tubespbp/ChangeProfileActivity.java
+++ app/src/main/java/com/tubesb/tubespbp/ChangeProfileActivity.java
@@ -1,14 +1,19 @@
 package com.tubesb.tubespbp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +22,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangeProfileActivity extends AppCompatActivity {
 
@@ -28,6 +35,8 @@ public class ChangeProfileActivity extends AppCompatActivity {
     private String nama = "";
     private String alamat = "";
     private String noTelp = "";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,8 @@ public class ChangeProfileActivity extends AppCompatActivity {
         inputNoTelp = findViewById(R.id.inputNoTelp);
         btnSumbit = findViewById(R.id.btnSubmit);
         btnCancel = findViewById(R.id.btnCancel);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
         inputNama.setText(nama);
         inputAlamat.setText(alamat);
@@ -117,5 +128,47 @@ public class ChangeProfileActivity extends AppCompatActivity {
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(0, builder.build());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu_dashboard){
+            startActivity(new Intent(this, ProfileActivity.class));
+            finish();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Apa Kamu yakin untuk Keluar?");
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "Ya", then he is allowed to exit from application
+                firebaseAuth.signOut();
+                finish();
+            }
+        });
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "Tidak", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
